4a0296457c865327a6cefd9155ecd8647616f0d5
khushi <khushiverma1708@gmail.com>
2024-03-15 00:48:09 +0530

    added

--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -1,15 +1,39 @@
-import React from "react";
+import {React,useState} from "react";
 import './myStyles.css';
 import Sidebar from "./Sidebar"
 import ChatArea from "./ChatArea";
 import ConversationsItem from "./ConversationsItem";
 import Welcome from "./Welcome";
+import { Outlet } from "react-router-dom";
+
 function MainContainer(){
+    const [conversations, setConversations]= useState([
+        {
+            Name: "Test1",
+            lastMessage: "Last Message #1",
+            timeStamp: "Today",
+        },
+        {
+            Name: "Test2",
+            lastMessage: "Last Message #2",
+            timeStamp: "Today",
+        },
+        {
+            Name: "Test3",
+            lastMessage: "Last Message #3",
+            timeStamp: "Today",
+        }
+    ])
     return(
         <div className="main-container">
             <Sidebar/>
-            <ChatArea props={ConversationsItem[0]}/>
+            {/* outlet is component of library which helps to render out components inside another components */}
+            <Outlet/>
+            {/* <ChatArea props={ConversationsItem[0]}/> */}
             {/* <Welcome/> */}
+            {/* <ChatArea props={conversations[0]}/> */}
+            {/* <Users></Users> */}
+
 
         </div>
     )
